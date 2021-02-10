5464aab3d11d0c180a5380b25ac520d9a4df5acd
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-10 11:52:35 -0500

    replacing old repository due to heroku naming bug

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "ead8307a-7c80-47bc-b3b1-d5e3edcebeaa";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
